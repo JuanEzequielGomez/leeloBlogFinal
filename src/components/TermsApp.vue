824708d1409dd85371d7525e64b7de9b3212148f
Ezequiel Gomez <juanezequielgomez95@gmail.com>
2022-12-14 19:27:32 -0300

    second update modified terms

--- src/components/TermsApp.vue
+++ src/components/TermsApp.vue
@@ -38,7 +38,7 @@
                                 By using the Services, you state that:
                                 <ul class="enlisted">
                                     <li>You are at least 13 years old and over the minimum age required by the laws of your country of residence to access and use the Services;</li>
-                                    <li>You can form a binding contract with Reddit, or, if you are over 13 but under the age of majority in your jurisdiction, that your legal guardian has reviewed and agrees to these Terms;</li>
+                                    <li>You can form a binding contract with Leelo, or, if you are over 13 but under the age of majority in your jurisdiction, that your legal guardian has reviewed and agrees to these Terms;</li>
                                     <li>You are not barred from using the Services under all applicable laws; and</li>
                                     <li>You have not been permanently suspended or removed from the Services.</li>
                                 </ul>
@@ -80,12 +80,12 @@
                     data-bs-parent="#accordionFlushExample">
                     <div class="accordion-body">
                         <ol>
-                            <li>Subject to your complete and ongoing compliance with these Terms, Reddit grants you a personal, non-transferable, non-exclusive, revocable, limited license to: (a) install and use a copy of our mobile application associated with the Services that is obtained from a legitimate marketplace on a mobile device owned or controlled by you; and (b) access and use the Services. We reserve all rights not expressly granted to you by these Terms.</li>
+                            <li>Subject to your complete and ongoing compliance with these Terms, Leelo grants you a personal, non-transferable, non-exclusive, revocable, limited license to: (a) install and use a copy of our mobile application associated with the Services that is obtained from a legitimate marketplace on a mobile device owned or controlled by you; and (b) access and use the Services. We reserve all rights not expressly granted to you by these Terms.</li>
                             <li>Except and solely to the extent such a restriction is impermissible under applicable law, you may not, without our written agreement:
                                 <ul class="enlisted">
                                     <li>license, sell, transfer, assign, distribute, host, or otherwise commercially exploit the Services or Content;</li>
                                     <li>modify, prepare derivative works of, disassemble, decompile, or reverse engineer any part of the Services or Content; or</li>
-                                    <li>access the Services or Content in order to build a similar or competitive website, product, or service, except as permitted under the Reddit API Terms of Use.</li>
+                                    <li>access the Services or Content in order to build a similar or competitive website, product, or service, except as permitted under the Leelo API Terms of Use.</li>
                                 </ul>
                             </li>
                             <li>
@@ -108,10 +108,10 @@
                     <div class="accordion-body">
                         <ul>
                             <li>
-                                To use certain features of our Services, you may be required to create a Reddit account (an “Account”) and provide us with a username, password, and certain other information about yourself as set forth in the Privacy Policy.
+                                To use certain features of our Services, you may be required to create a Leelo account (an “Account”) and provide us with a username, password, and certain other information about yourself as set forth in the Privacy Policy.
                             </li>
                             <li>
-                                You are solely responsible for the information associated with your Account and anything that happens related to your Account. You must maintain the security of your Account and immediately notify Reddit if you discover or suspect that someone has accessed your Account without your permission. We recommend that you use a strong password that is used only with your Account and enable two-factor authentication.
+                                You are solely responsible for the information associated with your Account and anything that happens related to your Account. You must maintain the security of your Account and immediately notify Leelo if you discover or suspect that someone has accessed your Account without your permission. We recommend that you use a strong password that is used only with your Account and enable two-factor authentication.
                             </li>
                             <li>
                                 You will not license, sell, or transfer your Account without our prior written approval.
@@ -139,13 +139,13 @@
                                 By submitting Your Content to the Services, you represent and warrant that you have all rights, power, and authority necessary to grant the rights to Your Content contained within these Terms. Because you alone are responsible for Your Content, you may expose yourself to liability if you post or share Content without all necessary rights.
                             </li>
                             <li>
-                                You retain any ownership rights you have in Your Content, but you grant Reddit the following license to use that Content:
+                                You retain any ownership rights you have in Your Content, but you grant Leelo the following license to use that Content:
                             </li>
                             <li>
-                                When Your Content is created with or submitted to the Services, you grant us a worldwide, royalty-free, perpetual, irrevocable, non-exclusive, transferable, and sublicensable license to use, copy, modify, adapt, prepare derivative works of, distribute, store, perform, and display Your Content and any name, username, voice, or likeness provided in connection with Your Content in all media formats and channels now known or later developed anywhere in the world. This license includes the right for us to make Your Content available for syndication, broadcast, distribution, or publication by other companies, organizations, or individuals who partner with Reddit. You also agree that we may remove metadata associated with Your Content, and you irrevocably waive any claims and assertions of moral rights or attribution with respect to Your Content.
+                                When Your Content is created with or submitted to the Services, you grant us a worldwide, royalty-free, perpetual, irrevocable, non-exclusive, transferable, and sublicensable license to use, copy, modify, adapt, prepare derivative works of, distribute, store, perform, and display Your Content and any name, username, voice, or likeness provided in connection with Your Content in all media formats and channels now known or later developed anywhere in the world. This license includes the right for us to make Your Content available for syndication, broadcast, distribution, or publication by other companies, organizations, or individuals who partner with Leelo. You also agree that we may remove metadata associated with Your Content, and you irrevocably waive any claims and assertions of moral rights or attribution with respect to Your Content.
                             </li>
                             <li>
-                                Any ideas, suggestions, and feedback about Reddit or our Services that you provide to us are entirely voluntary, and you agree that Reddit may use such ideas, suggestions, and feedback without compensation or obligation to you.
+                                Any ideas, suggestions, and feedback about Leelo or our Services that you provide to us are entirely voluntary, and you agree that Leelo may use such ideas, suggestions, and feedback without compensation or obligation to you.
                             </li>
                             <li>
                                 Although we have no obligation to screen, edit, or monitor Your Content, we may, in our sole discretion, delete or remove Your Content at any time and for any reason, including for violating these Terms, violating our Content Policy, or if you otherwise create or are likely to create liability for us.
